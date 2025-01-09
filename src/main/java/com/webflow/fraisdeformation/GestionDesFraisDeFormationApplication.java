944ad34342f492696a616672e54b61ede5c39c29
adrie <adrien.carles@limayrac.fr>
2025-01-09 13:41:15 +0100

    Initial commit

---
+++ src/main/java/com/webflow/fraisdeformation/GestionDesFraisDeFormationApplication.java
@@ -0,0 +1,13 @@
+package com.webflow.fraisdeformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesFraisDeFormationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDesFraisDeFormationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webflow/fraisdeformation/GestionDesFraisDeFormationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflow.fraisdeformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesFraisDeFormationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
