33727692a9fa81488d79076b0a331c20e3ad39fc
adrie <adrien.carles@limayrac.fr>
2025-02-02 17:13:42 +0100

    workflow 2

--- src/main/java/com/webflow/fraisdeformation/controller/DeclarationController.java
+++ src/main/java/com/webflow/fraisdeformation/controller/DeclarationController.java
@@ -2,12 +2,14 @@ package com.webflow.fraisdeformation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/declaration")
 public class DeclarationController {
 
-    @GetMapping("/declaration")
+    @GetMapping("")
     public String lancerDeclaration() {
-        return "declaration-flow";
+        return "redirect:/flows/declaration-flow";
     }
 }
